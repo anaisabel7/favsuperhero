97f94c6d37c477127dbd6f683ae6eab0034e5c2a
Ana Isabel <imacswv@gmail.com>
2018-08-28 13:44:29 +0200

    Added slug to the Superhero model

--- favsuperhero/supers/models.py
+++ favsuperhero/supers/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
 
 class User(AbstractUser):
@@ -9,5 +10,6 @@ class User(AbstractUser):
 
 class Superhero(models.Model):
 
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, unique=True)
     player = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    slug = AutoSlugField(populate_from='name')
