3029384b87dffca00ef4ea91d43bc3ff43729cf9
Ana Isabel <imacswv@gmail.com>
2018-08-30 10:10:03 +0200

    Basic list view of superheroes by player

--- favsuperhero/supers/urls.py
+++ favsuperhero/supers/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from supers.views import SuperheroDetailView
+from supers.views import SuperheroDetailView, SuperheroPlayerListView
 
 urlpatterns = [
     path(
         'superhero/<slug:slug>/',
         SuperheroDetailView.as_view(),
         name='superhero_detail'
+    ),
+    path(
+        'superheroes_by_player/<player>/',
+        SuperheroPlayerListView.as_view(),
+        name='superhero_list_by_player'
     )
 ]
--- favsuperhero/supers/views.py
+++ favsuperhero/supers/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from supers.models import Superhero
+from django.views.generic.list import ListView
+from supers.models import Superhero, User
 
 
 class SuperheroDetailView(DetailView):
     template_name = 'supers/superhero_detail.html'
     model = Superhero
+
+
+class SuperheroPlayerListView(ListView):
+    template_name = 'supers/superhero_list.html'
+    model = Superhero
+
+    def get(self, request, *args, **kwargs):
+        self.player = kwargs['player']
+        return super().get(self, request, *args, **kwargs)
+
+    def get_queryset(self):
+        player = User.objects.filter(username=self.player)[0]
+        return Superhero.objects.filter(player=player)
